1fe955fc76de51271a6c9d93eb37648eb327b572
Tommaso Musetti <musettitommaso@gmail.com>
2026-08-20 14:24:30 +0200

    Update Venue.php

--- app/Domain/Catalog/Models/Venue.php
+++ app/Domain/Catalog/Models/Venue.php
@@ -2,9 +2,17 @@
 
 namespace App\Domain\Catalog\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['name', 'region', 'city', 'address', 'latitude', 'longitude'])]
 class Venue extends Model
 {
-    //
+    protected function casts(): array
+    {
+        return [
+            'latitude' => 'decimal:7',
+            'longitude' => 'decimal:7',
+        ];
+    }
 }
